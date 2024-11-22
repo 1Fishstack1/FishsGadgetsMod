fe18a51915c5010c88788e1fa991b8a3563e7f7f
FishStack <64903322+1Fishstack1@users.noreply.github.com>
2024-11-22 23:44:49 +0100

    first git test

--- src/main/java/net/fishstack/fishsgadgets/FishsGadgetsMod.java
+++ src/main/java/net/fishstack/fishsgadgets/FishsGadgetsMod.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FishsGadgetsMod.MOD_ID)
 public class FishsGadgetsMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "fishsgadgets";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
