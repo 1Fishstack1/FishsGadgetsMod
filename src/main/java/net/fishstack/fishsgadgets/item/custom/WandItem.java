2e595c9c8ce8ec8fbefcae45b75515b60eb62ab5
FishStack <64903322+1Fishstack1@users.noreply.github.com>
2024-11-29 20:44:52 +0100

    First custom particle test

--- src/main/java/net/fishstack/fishsgadgets/item/custom/WandItem.java
+++ src/main/java/net/fishstack/fishsgadgets/item/custom/WandItem.java
@@ -13,6 +13,15 @@ import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import team.lodestar.lodestone.registry.common.particle.LodestoneParticleRegistry;
+import team.lodestar.lodestone.systems.easing.Easing;
+import team.lodestar.lodestone.systems.particle.builder.WorldParticleBuilder;
+import team.lodestar.lodestone.systems.particle.data.GenericParticleData;
+import team.lodestar.lodestone.systems.particle.data.color.ColorParticleData;
+import team.lodestar.lodestone.systems.particle.data.spin.SpinParticleData;
+import team.lodestar.lodestone.systems.particle.render_types.LodestoneWorldParticleRenderType;
+
+import java.awt.*;
 
 public class WandItem extends Item {
 
@@ -26,9 +35,10 @@ public class WandItem extends Item {
 
         BlockHitResult blockhitresult = getPlayerViewedBlockResult(pLevel, pPlayer, ClipContext.Fluid.NONE);
         BlockPos lookPos = blockhitresult.getBlockPos().above();
-        pLevel.explode(pPlayer, lookPos.getX(), lookPos.getY(), lookPos.getZ(), (float) explosionRadius, Level.ExplosionInteraction.TNT);
+        // pLevel.explode(pPlayer, lookPos.getX(), lookPos.getY(), lookPos.getZ(), (float) explosionRadius, Level.ExplosionInteraction.TNT);
+        spawnTestParticles(pLevel, lookPos);
 
-        pPlayer.getCooldowns().addCooldown(this, 100);
+        // pPlayer.getCooldowns().addCooldown(this, 100);
 
         return super.use(pLevel, pPlayer, pUsedHand);
     }
@@ -47,5 +57,19 @@ public class WandItem extends Item {
         float f7 = f2 * f4;
         Vec3 vec31 = vec3.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
         return pLevel.clip(new ClipContext(vec3, vec31, ClipContext.Block.OUTLINE, pFluidMode, pPlayer));
+    }
+
+    public static void spawnTestParticles(Level pLevel, BlockPos pPos) {
+        Color startingColor = new Color(0, 0, 0);
+        Color endingColor = new Color(104, 104, 104);
+        WorldParticleBuilder.create(LodestoneParticleRegistry.WISP_PARTICLE)
+                .setRenderType(LodestoneWorldParticleRenderType.LUMITRANSPARENT)
+                .setScaleData(GenericParticleData.create(1, 5, 1).build())
+                .setTransparencyData(GenericParticleData.create(1, 0.25f).build())
+                .setColorData(ColorParticleData.create(startingColor, endingColor).setCoefficient(1.4f).setEasing(Easing.QUARTIC_OUT).build())
+                .setLifetime(200)
+                .addMotion(0, 0.01f, 0)
+                .enableNoClip()
+                .spawn(pLevel, pPos.getX() + 0.5, pPos.getY() + 0.5, pPos.getZ() + 0.5);
     }
 }
